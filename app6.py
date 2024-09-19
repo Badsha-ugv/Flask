d3db79448fd1ae7b6611b175057a9ae5f705c1f7
badsha-ugv <badsha.ugv@gmail.com>
2024-09-19 12:33:53 +0600

    flash message

---
+++ app6.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, flash, request, redirect
+app = Flask(__name__)
+app.secret_key = 'my secret'
+
+
+@app.route('/')
+def index():
+   
+    return render_template('flashmsg.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'admin' and password == 'admin':
+            flash('login success')
+            return redirect('/')
+        else:
+            flash('lgin failed')
+            return redirect('/')
